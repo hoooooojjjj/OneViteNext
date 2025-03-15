c93037b97664c6eb3a0d70262cf6de11af53382c
ryuhojun <rhj080471@gmail.com>
2025-03-15 22:59:27 +0900

    Revert "페이지 라우팅 설정하기"
    This reverts commit 4fe93ad1f598df710de01eff77dfa63978b24464.

--- section02/src/pages/index.tsx
+++ section02/src/pages/index.tsx
@@ -3,13 +3,5 @@
 import { trpc } from "@/utils/trpc";
 
 export default function Home() {
-  const hello = trpc.hello.useQuery("TRPC");
-
-  if (!hello.data) return <div>Loading...</div>;
-
-  return (
-    <div>
-      <h1>{hello.data}</h1>
-    </div>
-  );
+  return <div></div>;
 }

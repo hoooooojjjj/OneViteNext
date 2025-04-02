ce2f2025ccc5e59f02bbdab2a344c71adddf30d0
ryuhojun <rhj080471@gmail.com>
2025-04-02 23:43:06 +0900

    Feat : react-hook-form으로 search 기능 구현

--- section02/src/components/searchable-layout/index.tsx
+++ section02/src/components/searchable-layout/index.tsx
@@ -1,9 +1,34 @@
 import { ReactNode } from "react";
+import { SubmitHandler, useForm } from "react-hook-form";
+
+interface SearchInput {
+  searchValue: string;
+}
 
 function SearchableLayout({ children }: { children: ReactNode }) {
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm<SearchInput>();
+
+  const handleSubmitSearch: SubmitHandler<SearchInput> = (
+    data: SearchInput
+  ) => {
+    reset();
+  };
+
   return (
     <div>
-      <div>임시 서치 바</div>
+      <form onSubmit={handleSubmit(handleSubmitSearch)}>
+        <input
+          placeholder="검색어를 입력하세요."
+          {...register("searchValue")}
+        />
+        <input type="submit" value={"검색"} />
+        {errors.searchValue && <span>This field is required</span>}
+      </form>
       {children}
     </div>
   );

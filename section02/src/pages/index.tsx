e04f44f6d445fe3a67ab058f4454d99bc7381ecc
ryuhojun <rhj080471@gmail.com>
2025-04-03 22:43:36 +0900

    Home Section Header

--- section02/src/pages/index.tsx
+++ section02/src/pages/index.tsx
@@ -1,8 +1,18 @@
 import SearchableLayout from "@/components/searchable-layout";
 import { ReactNode } from "react";
+import { AllBooksSection, Container, Header, RecommendSection } from "./styles";
 
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <Container>
+      <RecommendSection>
+        <Header>지금 추천하는 도서</Header>
+      </RecommendSection>
+      <AllBooksSection>
+        <Header>등록된 모든 도서</Header>
+      </AllBooksSection>
+    </Container>
+  );
 }
 
 Home.getLayout = (page: ReactNode) => {
---
+++ section02/src/pages/styles.ts
@@ -0,0 +1,15 @@
+import styled from "@emotion/styled";
+
+export const Container = styled.div({
+  display: "flex",
+  flexDirection: "column",
+  gap: 20,
+});
+
+export const RecommendSection = styled.section({});
+
+export const AllBooksSection = styled.section({});
+
+export const Header = styled.h3({
+  marginBottom: 0,
+});

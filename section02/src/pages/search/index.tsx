1633af5b4a40e14b9c0b9c090769010f5bd0d586
ryuhojun <rhj080471@gmail.com>
2025-04-03 23:02:15 +0900

    Search Page Styling

--- section02/src/pages/search/index.tsx
+++ section02/src/pages/search/index.tsx
@@ -1,11 +1,19 @@
 import SearchableLayout from "@/components/searchable-layout";
 import { useRouter } from "next/router";
 import { ReactNode } from "react";
+import BookItem from "@/components/book-item";
+import books from "@/mock/books.json";
 
 export default function Search() {
   const router = useRouter();
   const q = router.query.q;
-  return <div>Search : {q}</div>;
+  return (
+    <div>
+      {books.map((book) => (
+        <BookItem key={book.id} {...book}></BookItem>
+      ))}
+    </div>
+  );
 }
 
 Search.getLayout = (page: ReactNode) => {
